d18e0dd4f979084dd95ac22aaeab63f3104d5816
Maria Isabel <sakura.mabahi@gmail.com>
2024-01-08 22:46:47 -0300

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://1a1feb33-63c2-49d6-b6aa-fc69c8fdbe4a.serverhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
